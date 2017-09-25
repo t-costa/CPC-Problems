048b9941b86a0475a0a6c1be0f184127d6a29c55
t-costa <tommasocostanzo93@gmail.com>
2017-09-25 23:07:50 +0200

    Update Towers.cpp

--- Towers/Towers.cpp
+++ Towers/Towers.cpp
@@ -8,19 +8,18 @@
 
 void towers(std::vector<int> v){
     std::unordered_map<int, int> table;
+    int max = 1;
     int total = 0;
 
     for (int i=0; i<v.size(); i++){
         if (table.find(v[i]) == table.end()){
             table.emplace(v[i], 1); //inserisco per la prima volta
             total++;
-        } else table[v[i]]++;
-    }
-
-    int max = table[v[0]];
-    for (auto it=table.begin(); it!=table.end(); ++it){
-        if (it->second > max)
-            max = it->second;
+        } else {
+            table[v[i]]++;
+            if (table[v[i]] > max)
+                max = table[v[i]];
+        }
     }
 
     std::cout << max << ' ' << total << std::endl;
