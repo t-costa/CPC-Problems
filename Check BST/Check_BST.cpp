15a1d9fc94e92df41a535cdfa3ed133e30693ca9
t-costa <tommasocostanzo93@gmail.com>
2017-10-13 22:46:55 +0200

    Update and rename Check_BST/Check_BST.cpp to Check BST/Check_BST.cpp

--- Check BST/Check_BST.cpp
+++ Check BST/Check_BST.cpp
@@ -16,15 +16,15 @@ int getMax(Node* n){
 }
 
 bool isBST(Node* root){
-    //in order visit
     if (root == nullptr)
         return true;
 
-    int minRight = 1000, maxLeft = 1;    //from constraint, if there is just a node I want true
+    int minRight = 1000, maxLeft = 1;    //values from constraint, if there is just a node I want true
     if (root->left != nullptr) maxLeft = getMax(root->left);
     if (root->right != nullptr) minRight = getMin(root->right);
 
-    if (minRight > root->data && maxLeft <= root->data) return isBST(root->left) && isBST(root->right);
+    if (minRight > root->data && maxLeft <= root->data) 
+        return isBST(root->left) && isBST(root->right);
     else return false;
 
 }
