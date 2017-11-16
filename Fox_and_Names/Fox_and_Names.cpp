fc7bbffa583a8dd4abb98956083fadf23dd0833d
t-costa <tommasocostanzo93@gmail.com>
2017-11-16 22:52:24 +0100

    Adding Fox_and_Names.cpp

---
+++ Fox_and_Names/Fox_and_Names.cpp
@@ -0,0 +1,114 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <queue>
+
+
+struct node{
+    char value;
+    int has_incoming;
+    std::vector<node*> children;
+};
+
+//to access position for character a -> a-97
+
+std::set<char> alphabet;
+std::vector<node> new_alphabet;
+std::vector<char> result;
+bool error = false;
+
+bool find(std::vector<node*> const& v, char c) {
+    bool found = false;
+    int i=0;
+    if (v.empty()) return false;
+
+    while (i<v.size() && !found) {
+        if (v[i]->value == c)
+            found = true;
+        i++;
+    }
+
+    return found;
+}
+
+void build_alphabet(std::string const& a, std::string const& b) {
+    size_t i=0;
+    size_t shorter = std::min(a.size(), b.size());
+
+    while (i<shorter && a[i]==b[i])
+        i++;
+
+    if (i < shorter) {
+        //different character found, a[i] precedes b[i]
+        if (!find(new_alphabet[(a[i]-97)].children, b[i])) {
+            new_alphabet[(a[i]-97)].children.push_back(&new_alphabet[(b[i]-97)]);
+            new_alphabet[(b[i]-97)].has_incoming++;
+        }
+    } else {
+        if (shorter == b.size() && shorter < a.size())
+            //all the characters are equal, and b is smaller than a
+            error = true;
+    }
+}
+
+bool topological_sort() {
+
+    std::queue<node*> q;
+    size_t counter = 0;
+
+    for (auto& n : new_alphabet) {
+        if (n.has_incoming == 0)
+            q.push(&n);
+    }
+    //q contains all nodes with no incoming edge
+
+    while (!q.empty()) {
+        node* n = q.front();
+        q.pop();
+        result.push_back(n->value);
+        for (auto& c : n->children) {
+            c->has_incoming--; //logically remove edge
+            if (c->has_incoming == 0)
+                q.push(c);
+        }
+        counter++;
+    }
+
+    return (counter == 26); //all the letters of the alphabet
+}
+
+int main() {
+    std::vector<std::string> words;
+
+    size_t n;
+    std::cin >> n;
+    words.reserve(n);
+
+    for (int i=97; i<123; ++i){
+        alphabet.insert((char) i);
+        new_alphabet.emplace_back(node {(char) i, 0, {}});
+    }
+
+    for (int i=0; i<n; ++i) {
+        std::string s;
+        std::cin >> s;
+        words.push_back(s);
+    }
+
+    for (int i=0; i<n-1; ++i) {
+        build_alphabet(words[i], words[i+1]);
+        if (error) {
+            std::cout << "Impossible";
+            return 0;
+        }
+    }
+
+    if (topological_sort())
+        for (auto r : result)
+            std::cout << r;
+    else
+        std::cout << "Impossible";
+
+    return 0;
+}
