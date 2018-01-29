345f977ef60182479591720e62d9427dcff65de3
Tommaso Costanzo <tommasocostanzo93@gmail.com>
2018-01-29 23:06:45 +0100

    Optimization

--- Longest Bitonic Subsequence/Longest_Bitonic_Subsequence.cpp
+++ Longest Bitonic Subsequence/Longest_Bitonic_Subsequence.cpp
@@ -20,8 +20,53 @@ std::vector<int> lis(std::vector<int> const& s) {
         lis.push_back(max+1);   //if there is no j s.t. lis[j]>max, max is 0 -> ok
     }
 
-
     return lis;
+}
+
+int binary_search(std::vector<int> const& v, int l, int r, int key) {
+
+    while (r-l > 1) {
+        int m = l + (r-l)/2;
+        if (v[m] >= key)
+            r = m;
+        else
+            l = m;
+    }
+
+    return r;
+}
+
+std::vector<int> optimized_lis(std::vector<int> const& s) {
+
+    std::vector<int> lis;
+    std::vector<int> inc;
+    lis.reserve(s.size());
+    inc.reserve(s.size());
+
+    lis.push_back(s[0]);
+    inc.push_back(1);
+    int length = 1;
+    for (int i=1; i<s.size(); ++i) {
+        if (s[i] < lis.front()) {
+            //new smallest element
+            lis.front() = s[i];
+            inc.push_back(1);
+        } else {
+            if (s[i] > lis.back()) {
+                //biggest element increase longest sequence
+                lis.push_back(s[i]);
+                length++;
+                inc.push_back(length);
+            } else {
+                //element in the middle, search right position
+                int index = binary_search(lis, -1, length-1, s[i]);
+                lis[index] = s[i];
+                inc.push_back(index+1);
+            }
+        }
+    }
+
+    return inc;
 }
 
 int main() {
@@ -40,11 +85,12 @@ int main() {
             s.push_back(val);
         }
 
-        std::vector<int> inc = lis(s);
+        std::vector<int> inc = optimized_lis(s);
 
         std::reverse(s.begin(), s.end());
-        std::vector<int> dec = lis(s);
-        
+
+        std::vector<int> dec = optimized_lis(s);
+
         std::reverse(dec.begin(), dec.end());
 
         int max = inc[0] + dec[0];
